ae8e4cd175b9cbae9f3f0d7b17bb5ad1cc4b70f5
Iman Kazerani <kazeraniman@gmail.com>
2019-12-26 23:25:12 -0500

    Even more opcodes and tests.  A third of the way through all opcodes now.

--- src/emulator.py
+++ src/emulator.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+from typing import List
+
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
@@ -10,6 +12,7 @@ logger.disabled = "pydevd" not in sys.modules
 UPPER_CHAR_MASK = 240
 LOWER_CHAR_MASK = 15
 GAME_START_ADDRESS = 512
+RETURN_FROM_SUBROUTINE_OPCODE = bytes.fromhex("00EE")
 
 
 class Emulator:
@@ -17,6 +20,7 @@ class Emulator:
         self.ram = bytearray(4096)
         self.registers = bytearray(16)
         self.program_counter = GAME_START_ADDRESS
+        self.stack: List[int] = []
 
     def load_game(self, path: Path) -> None:
         """
@@ -78,8 +82,12 @@ class Emulator:
         first_char = self.get_upper_char(opcode[0])
         last_char = self.get_lower_char(opcode[1])
 
-        if first_char == 1:
+        if opcode == RETURN_FROM_SUBROUTINE_OPCODE:
+            self.opcode_return_from_subroutine(opcode)
+        elif first_char == 1:
             self.opcode_goto(opcode)
+        elif first_char == 2:
+            self.opcode_call_subroutine(opcode)
         elif first_char == 3:
             self.opcode_if_equal(opcode)
         elif first_char == 4:
@@ -90,8 +98,28 @@ class Emulator:
             self.opcode_set_register_value(opcode)
         elif first_char == 7:
             self.opcode_add_value(opcode)
+        elif first_char == 8 and last_char == 0:
+            self.opcode_set_register_value_other_register(opcode)
+        elif first_char == 8 and last_char == 1:
+            self.opcode_set_register_bitwise_or(opcode)
+        elif first_char == 8 and last_char == 2:
+            self.opcode_set_register_bitwise_and(opcode)
+        elif first_char == 8 and last_char == 3:
+            self.opcode_set_register_bitwise_xor(opcode)
         else:
             logger.error(f"Unimplemented / Invalid Opcode: {opcode.hex()}.")
+
+    def opcode_return_from_subroutine(self, opcode: bytes) -> None:
+        """
+        Return from the current subroutine.
+        :param opcode: The opcode to execute.
+        """
+        if len(self.stack) == 0:
+            logger.error("Tried to return from a subroutine when the stack is empty.  Ignoring.")
+            return
+
+        self.program_counter = self.stack.pop()
+        logger.debug(f"Execute Opcode {opcode.hex}: Return from subroutine, continue at {hex(self.program_counter)}.")
 
     def opcode_goto(self, opcode: bytes) -> None:
         """
@@ -100,7 +128,17 @@ class Emulator:
         """
         address = (self.get_lower_char(opcode[0]) << 8) + opcode[1]
         self.program_counter = address
-        logger.debug(f"Execute Opcode {opcode.hex}: Jump to address {hex(address)}")
+        logger.debug(f"Execute Opcode {opcode.hex}: Jump to address {hex(address)}.")
+
+    def opcode_call_subroutine(self, opcode: bytes) -> None:
+        """
+        Call the subroutine at the given address.
+        :param opcode: The opcode to execute.
+        """
+        address = (self.get_lower_char(opcode[0]) << 8) + opcode[1]
+        self.stack.append(self.program_counter)
+        self.program_counter = address
+        logger.debug(f"Execute Opcode {opcode.hex}: Call subroutine at address {hex(address)}.")
 
     def opcode_if_equal(self, opcode: bytes) -> None:
         """
@@ -163,4 +201,54 @@ class Emulator:
         register = self.get_lower_char(opcode[0])
         self.registers[register] = (self.registers[register] + opcode[1]) % 256
         logger.debug(f"Execute Opcode {opcode.hex}: Add {opcode[1]} to the value of register {register}.")
+
+    def opcode_set_register_value_other_register(self, opcode: bytes) -> None:
+        """
+        Set the value of the first provided register to the value of the second provided register.
+        :param opcode: The opcode to execute.
+        """
+        first_register = self.get_lower_char(opcode[0])
+        second_register = self.get_upper_char(opcode[1])
+        second_register_value = self.registers[second_register]
+        self.registers[first_register] = second_register_value
+        logger.debug(f"Execute Opcode {opcode.hex}: Set the value of register {first_register} to the value of register {second_register}'s value ({second_register_value}).")
+
+    def opcode_set_register_bitwise_or(self, opcode: bytes) -> None:
+        """
+        Sets the value of the first provided register to the bitwise or of itself and the value of the second provided register.
+        :param opcode: The opcode to execute.
+        """
+        first_register = self.get_lower_char(opcode[0])
+        second_register = self.get_upper_char(opcode[1])
+        first_register_value = self.registers[first_register]
+        second_register_value = self.registers[second_register]
+        result = first_register_value | second_register_value
+        self.registers[first_register] = result
+        logger.debug(f"Execute Opcode {opcode.hex}: Set the value of register {first_register} to the bitwise or of itself and the value of register {second_register} ({first_register_value} | {second_register_value} = {result}).")
+
+    def opcode_set_register_bitwise_and(self, opcode: bytes) -> None:
+        """
+        Sets the value of the first provided register to the bitwise and of itself and the value of the second provided register.
+        :param opcode: The opcode to execute.
+        """
+        first_register = self.get_lower_char(opcode[0])
+        second_register = self.get_upper_char(opcode[1])
+        first_register_value = self.registers[first_register]
+        second_register_value = self.registers[second_register]
+        result = first_register_value & second_register_value
+        self.registers[first_register] = result
+        logger.debug(f"Execute Opcode {opcode.hex}: Set the value of register {first_register} to the bitwise and of itself and the value of register {second_register} ({first_register_value} & {second_register_value} = {result}).")
+
+    def opcode_set_register_bitwise_xor(self, opcode: bytes) -> None:
+        """
+        Sets the value of the first provided register to the bitwise xor of itself and the value of the second provided register.
+        :param opcode: The opcode to execute.
+        """
+        first_register = self.get_lower_char(opcode[0])
+        second_register = self.get_upper_char(opcode[1])
+        first_register_value = self.registers[first_register]
+        second_register_value = self.registers[second_register]
+        result = first_register_value ^ second_register_value
+        self.registers[first_register] = result
+        logger.debug(f"Execute Opcode {opcode.hex}: Set the value of register {first_register} to the bitwise xor of itself and the value of register {second_register} ({first_register_value} ^ {second_register_value} = {result}).")
     # endregion
--- test/emulator_test.py
+++ test/emulator_test.py
@@ -7,11 +7,41 @@ class TestIndividualOpcodes:
     def setup_method(self):
         self.emulator = Emulator()
 
+    def test_opcode_return_from_subroutine(self):
+        assert self.emulator.program_counter == GAME_START_ADDRESS, "Program counter starting at an unexpected value."
+        assert len(self.emulator.stack) == 0, "Stack starting out non-empty."
+
+        self.emulator.opcode_return_from_subroutine(bytes.fromhex("00EE"))
+        assert self.emulator.program_counter == GAME_START_ADDRESS, "Returning from a subroutine when not in one messed up the program counter."
+        assert len(self.emulator.stack) == 0, "Stack got into a weird state when trying to return from a subroutine when not in one."
+
+        self.emulator.stack = [2000, 3000]
+        self.emulator.opcode_return_from_subroutine(bytes.fromhex("00EE"))
+        assert self.emulator.program_counter == 3000, "Program counter set to wrong value when returning from a subroutine."
+        assert len(self.emulator.stack) == 1, "Stack entries incorrect after returning from a subroutine."
+
+        self.emulator.opcode_return_from_subroutine(bytes.fromhex("00EE"))
+        assert self.emulator.program_counter == 2000, "Program counter set to wrong value when returning from a subroutine."
+        assert len(self.emulator.stack) == 0, "Stack entries incorrect after returning from a subroutine."
+
     def test_opcode_goto(self):
         assert self.emulator.program_counter == GAME_START_ADDRESS, "Program counter starting at an unexpected value."
 
         self.emulator.opcode_goto(bytes.fromhex("14e5"))
         assert self.emulator.program_counter == int("4e5", 16), "Program counter incorrect after jump opcode."
+
+    def test_opcode_call_subroutine(self):
+        assert self.emulator.program_counter == GAME_START_ADDRESS, "Program counter starting at an unexpected value."
+        assert len(self.emulator.stack) == 0, "Stack starting out non-empty."
+
+        self.emulator.opcode_call_subroutine(bytes.fromhex("2578"))
+        assert self.emulator.program_counter == int("578", 16), "Program counter incorrect after subroutine call."
+        assert len(self.emulator.stack) == 1 and self.emulator.stack[0] == GAME_START_ADDRESS, "Previous program counter not added to the stack."
+
+        self.emulator.opcode_call_subroutine(bytes.fromhex("2a23"))
+        assert self.emulator.program_counter == int("a23", 16), "Program counter incorrect after subroutine call."
+        assert len(self.emulator.stack) == 2 and self.emulator.stack[1] == int("578", 16), "Previous program counter not added to the stack."
+        assert len(self.emulator.stack) == 2 and self.emulator.stack[0] == GAME_START_ADDRESS, "Earlier stack value was modified."
 
     def test_opcode_if_equal(self):
         assert self.emulator.program_counter == GAME_START_ADDRESS, "Program counter starting at an unexpected value."
@@ -75,6 +105,63 @@ class TestIndividualOpcodes:
         assert self.emulator.registers[11] == 9, "Register addition overflow did not work as expected."
         assert self.emulator.registers[15] == 0, "Carry bit was set when it should not be modified by this instruction."
 
+    def test_opcode_set_register_value_other_register(self):
+        for register in self.emulator.registers:
+            assert register == 0, "Register starting at an unexpected value."
+
+        self.emulator.registers[8] = 47
+        self.emulator.opcode_set_register_value_other_register(bytes.fromhex("8480"))
+        for index, register in enumerate(self.emulator.registers):
+            if index == 4 or index == 8:
+                assert register == 47, "Register not set to correct value."
+            else:
+                assert register == 0, "Different register than target had its value modified."
+
+    def test_opcode_set_register_bitwise_or(self):
+        for register in self.emulator.registers:
+            assert register == 0, "Register starting at an unexpected value."
+
+        self.emulator.registers[4] = 170
+        self.emulator.registers[8] = 85
+        self.emulator.opcode_set_register_bitwise_or(bytes.fromhex("8481"))
+        for index, register in enumerate(self.emulator.registers):
+            if index == 4:
+                assert register == 255, "Register not set to correct value."
+            elif index == 8:
+                assert register == 85, "Second register value was modified when it should not have been."
+            else:
+                assert register == 0, "Different register than target had its value modified."
+
+    def test_opcode_set_register_bitwise_and(self):
+        for register in self.emulator.registers:
+            assert register == 0, "Register starting at an unexpected value."
+
+        self.emulator.registers[4] = 204
+        self.emulator.registers[8] = 170
+        self.emulator.opcode_set_register_bitwise_and(bytes.fromhex("8482"))
+        for index, register in enumerate(self.emulator.registers):
+            if index == 4:
+                assert register == 136, "Register not set to correct value."
+            elif index == 8:
+                assert register == 170, "Second register value was modified when it should not have been."
+            else:
+                assert register == 0, "Different register than target had its value modified."
+
+    def test_opcode_set_register_bitwise_xor(self):
+        for register in self.emulator.registers:
+            assert register == 0, "Register starting at an unexpected value."
+
+        self.emulator.registers[4] = 204
+        self.emulator.registers[8] = 170
+        self.emulator.opcode_set_register_bitwise_xor(bytes.fromhex("8483"))
+        for index, register in enumerate(self.emulator.registers):
+            if index == 4:
+                assert register == 102, "Register not set to correct value."
+            elif index == 8:
+                assert register == 170, "Second register value was modified when it should not have been."
+            else:
+                assert register == 0, "Different register than target had its value modified."
+
 
 class TestOpcodeRouting:
     @classmethod
@@ -91,6 +178,12 @@ class TestOpcodeRouting:
     @mock.patch.object(Emulator, "opcode_goto")
     def test_goto(self, mock_method):
         opcode = bytes.fromhex("132a")
+        bad_opcode = bytes.fromhex("332a")
+        self.run_opcode(opcode, bad_opcode, mock_method)
+
+    @mock.patch.object(Emulator, "opcode_call_subroutine")
+    def test_call_subroutine(self, mock_method):
+        opcode = bytes.fromhex("232a")
         bad_opcode = bytes.fromhex("332a")
         self.run_opcode(opcode, bad_opcode, mock_method)
 
@@ -122,4 +215,34 @@ class TestOpcodeRouting:
     def test_add_value(self, mock_method):
         opcode = bytes.fromhex("7433")
         bad_opcode = bytes.fromhex("6133")
+        self.run_opcode(opcode, bad_opcode, mock_method)
+
+    @mock.patch.object(Emulator, "opcode_return_from_subroutine")
+    def test_return_from_subroutine(self, mock_method):
+        opcode = bytes.fromhex("00EE")
+        bad_opcode = bytes.fromhex("7433")
+        self.run_opcode(opcode, bad_opcode, mock_method)
+
+    @mock.patch.object(Emulator, "opcode_set_register_value_other_register")
+    def test_set_register_value_other_register(self, mock_method):
+        opcode = bytes.fromhex("8480")
+        bad_opcode = bytes.fromhex("00EE")
+        self.run_opcode(opcode, bad_opcode, mock_method)
+
+    @mock.patch.object(Emulator, "opcode_set_register_bitwise_or")
+    def test_set_register_bitwise_or(self, mock_method):
+        opcode = bytes.fromhex("8481")
+        bad_opcode = bytes.fromhex("8480")
+        self.run_opcode(opcode, bad_opcode, mock_method)
+
+    @mock.patch.object(Emulator, "opcode_set_register_bitwise_and")
+    def test_set_register_bitwise_and(self, mock_method):
+        opcode = bytes.fromhex("8482")
+        bad_opcode = bytes.fromhex("8481")
+        self.run_opcode(opcode, bad_opcode, mock_method)
+
+    @mock.patch.object(Emulator, "opcode_set_register_bitwise_xor")
+    def test_set_register_bitwise_xor(self, mock_method):
+        opcode = bytes.fromhex("8483")
+        bad_opcode = bytes.fromhex("8482")
         self.run_opcode(opcode, bad_opcode, mock_method)
